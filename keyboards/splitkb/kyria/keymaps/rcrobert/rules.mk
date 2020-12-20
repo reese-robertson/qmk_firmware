c4f4582a9667d0c08c138ee1840a35b21fdb7649
Reese Robertson <rrobertson@purestorage.com>
2020-12-20 11:12:11 -0800

    Create custom keymap

---
+++ keyboards/splitkb/kyria/keymaps/rcrobert/rules.mk
@@ -0,0 +1,5 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+# No encoders on my build
+# ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+# No RGB on my build
+# RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
